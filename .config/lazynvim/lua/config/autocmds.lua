3413758bc4bb0c2f048b00bce941d27aff8c89aa
leonamsh <llpdmonteiro@proton.me>
2025-07-23 07:47:58 -0300

    clean up some old/unused dotfiles

--- .config/lazynvim/lua/config/autocmds.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.opt_local.spell = true
-    vim.opt_local.spelllang = "pt_br" -- ou "en,pt_br" para múltiplos idiomas
-  end,
-})
