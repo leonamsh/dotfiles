33d9502f46c002085527e8f71f4389ab1a3927ec
leonamsh <llpdmonteiro@proton.me>
2025-07-23 16:47:38 -0300

    fix vim. in options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,60 +1,57 @@
--- Some keyboard mappings as I don't want to break my fingers, while typing on a "german" keyboard ;)
-vim.opt.langmap = "+]ü["
--- Plain langmap remapping does not seem to do the trick :(
-vim.keymap.set("n", "ü", "[", { remap = true })
+local opt = vim.opt
 
-vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
-vim.opt.tabstop = 4 -- How many spaces are shown per Tab
-vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
+opt.expandtab = true -- Convert tabs to spaces
+opt.shiftwidth = 4 -- Amount to indent with << and >>
+opt.tabstop = 4 -- How many spaces are shown per Tab
+opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
 
-vim.opt.smarttab = true
-vim.opt.smartindent = true
-vim.opt.autoindent = true -- Keep identation from previous line
+opt.smarttab = true
+opt.smartindent = true
+opt.autoindent = true -- Keep identation from previous line
 
 -- Enable break indent
-vim.opt.breakindent = true
+opt.breakindent = true
 
 -- Always show relative line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.wrap = false -- Disable wrap
-vim.opt.linebreak = true -- Companion to wrap, don't split words
+opt.wrap = true -- Disable wrap
+opt.linebreak = true -- Companion to wrap, don't split words
 
 -- Show line under cursor
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Store undos between sessions
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
+opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.showmode = false
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+opt.signcolumn = "yes"
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
--- Sets how neovim will display certain whitespace characters in the editor.
+-- Sets how neowill display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 5
+opt.scrolloff = 10
 
 -- Clipboard (scheduled to avoid startup performance impact)
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim
+    vim.opt.clipboard = "unnamedplus" -- Sync clipboard between OS and Neovim
 end)
