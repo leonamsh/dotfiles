a1783dbfd471fc25665c03ef635a199e5b6cda2e
leonamsh <lpdmonteiro@gmail.com>
2025-08-12 21:49:25 -0300

    fix: colorschemes e keymaps

---
+++ .config/qtile/scripts/dm-logout.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Script para menu de logout no Qtile usando dmenu/rofi
+# Dependências: rofi (ou dmenu), systemctl, slock (ou i3lock, etc.)
+
+# Variáveis de comando
+DMENU="rofi -dmenu -i -p 'Sair:'"
+# Altere "slock" para o seu comando de bloqueio de tela, se for diferente
+LOCKER="slock"
+
+# Opções do menu
+OPTIONS="Bloquear\nSair\nReiniciar\nDesligar\nSuspender"
+
+# Use o comando `echo` para passar as opções para o dmenu/rofi e capturar a escolha
+choice=$(echo -e "${OPTIONS}" | ${DMENU})
+
+case "$choice" in
+"Bloquear")
+  # Executa o comando de bloqueio de tela
+  ${LOCKER}
+  ;;
+"Sair")
+  # A maneira mais segura de sair do Qtile é com qtile-cmd
+  qtile-cmd -o cmd -f shutdown
+  ;;
+"Reiniciar")
+  # Comando do systemd para reiniciar o sistema
+  systemctl reboot
+  ;;
+"Desligar")
+  # Comando do systemd para desligar o sistema
+  systemctl poweroff
+  ;;
+"Suspender")
+  # Comando do systemd para suspender o sistema
+  systemctl suspend
+  ;;
+esac
