6128e3836d771ef9f19f75077fffba645d983a85
leonamsh <lpdmonteiro@gmail.com>
2025-08-14 22:22:43 -0300

    remoção do everforest e correção do erro

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,3 @@
--- Some keyboard mappings as I don't want to break my fingers, while typing on a "german" keyboard ;)
-vim.opt.langmap = "+]ü["
--- Plain langmap remapping does not seem to do the trick :(
-vim.keymap.set("n", "ü", "[", { remap = true })
-
 vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
 vim.opt.tabstop = 4 -- How many spaces are shown per Tab
